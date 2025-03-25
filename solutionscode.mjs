32fdc41a34c2b134501ab37bca72ad323417c5a6
Giorgio Fabbroni <giorgiofabbroni@hotmail.com>
2025-03-25 17:06:00 +0100

    made a function that retreivesthe Upper case letters out of the poem answer is silver

--- solutionscode.mjs
+++ solutionscode.mjs
@@ -1,3 +1,4 @@
+//CHALLENGE 1
 const symbolTable = {
     '☉': 'Gold',    
     '☿': 'Quicksilver',  
@@ -25,3 +26,22 @@ const symbolString = "☉☿☽♂☉";
 const decodedCode = decodeSymbols(symbolString);
 
 console.log("Decoded Code:", decodedCode);
+
+//CHALLENGE 2
+function solveAcrostic(poem) {
+    let acrostic = '';
+    for (let char of poem) {
+        if (char === char.toUpperCase() && char !== ' ') {
+            acrostic += char;
+        }
+    }
+    return acrostic;
+}
+
+const poem = `
+Still flows the Icy Lethe Veiling all neath Eldritch Rime
+`;
+
+const solution = solveAcrostic(poem);
+
+console.log("Acrostic Solution:", solution);
